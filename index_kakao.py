2946c637a609c0921de00d4556b1968fb8b9affe
rlaalstn0952 <91663592+rlaalstn0952@users.noreply.github.com>
2022-05-17 09:09:49 +0900

    Create index_kakao.py

---
+++ index_kakao.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index</title>
+</head>
+<body>
+<a
+href="https://kauth.kakao.com/oauth/authorize?client_id=4c1df285325e067dd3af5e4e9ace28a0&redirect_uri=http://localhost:7777/oauth&response_type=code">
+<img src="http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a4afa-a88b-989cf2a50476.jpg" width="300">
+</a>
+</body>
+</html>
